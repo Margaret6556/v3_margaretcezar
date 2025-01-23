bb5213890c81c8e5d455f3ff67159948d41557ae
Margaret Cezar <99307243+Margaret6556@users.noreply.github.com>
2025-01-22 23:13:40 -0500

    added json package

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>hello</h1>
   </React.StrictMode>
 );
-
